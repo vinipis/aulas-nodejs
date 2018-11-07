0a6583a6ce0fdc71015e92f7dfb27311ecd60dd3
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2018-11-07 14:49:31 -0200

    Estruturando os codigos em JS

--- primeiro-arquivo-js/app.js
+++ primeiro-arquivo-js/app.js
@@ -1,22 +1,9 @@
-var express = require('express');
-var msg = require('./mod_test');
+var app = require('./config/server');
 
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res){
-    res.render("home/index");
-});
-
-app.get('/formulario-inclusao-noticia', function(req, res){
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
-});
+var rotaHome = require('./app/routes/home')(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaFormularioInclusaoNoticia = require('./app/routes/formulario-inclusao-noticia')(app);
 
 app.listen(3000, function(){
-    console.log(msg());
+    console.log("Servidor ON");
 });
--- primeiro-arquivo-js/noticias.js
+++ primeiro-arquivo-js/noticias.js
@@ -1,9 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req,res){
-
     var categoria = req.url;
-    
     if(categoria == '/tecnologia'){
         res.end("<html><body>Notícias de Tecnologia</body></html>");
     }else if(categoria == '/moda'){
