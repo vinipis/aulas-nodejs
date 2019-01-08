89f192fb5425c67c7c244de628b0f26243b51fa3
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2019-01-08 14:58:23 -0200

    intalação do cosign e atualização do codigo para definição de rotas

--- primeiro-arquivo-js/app.js
+++ primeiro-arquivo-js/app.js
@@ -1,8 +1,8 @@
 var app = require('./config/server');
 
-var rotaHome = require('./app/routes/home')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaFormInclusaoNoticia = require('./app/routes/formulario-inclusao-noticia')(app);
+//var rotaHome = require('./app/routes/home')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+//var rotaFormInclusaoNoticia = require('./app/routes/formulario-inclusao-noticia')(app);
 
 app.listen(3000, function(){
     console.log("Servidor ON");
