5fd0bbae6a6cfd208d39b1d7fbd9d9c6ea688939
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2019-01-08 14:38:23 -0200

    separando o bnanco de dados e colocando ele nas configurações

--- primeiro-arquivo-js/app/routes/noticias.js
+++ primeiro-arquivo-js/app/routes/noticias.js
@@ -1,17 +1,13 @@
-module.exports = function(app){
+var dbConnection = require('../../config/dbConnection');
+
+module.exports = function(app) {
+
+    var connection = dbConnection();
+
     app.get('/noticias', function(req,res){
-
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host:       'localhost',
-            user:       'root',
-            password:   'GomariaDB',
-            database:   'portal_noticias'
-        });
 
         connection.query("SELECT * FROM noticias", function(error, result){
             res.render('noticias/noticias', { noticias : result });
         });
     });
-}
+};
